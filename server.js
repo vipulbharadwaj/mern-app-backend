69d6a949f492092666a584a81de0ad46fa6bc623
Vipul Chaudhary <thevipulbharadwaj@gmail.com>
2024-06-24 18:27:21 +0530

    Updated vercel

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const corsOptions = {
     origin: [
         "http://localhost:5173",
-        "https://vip-tech.netlify.app",
+        "https://mern-app-frontend-blue.vercel.app",
         "https://mern-app-backendd.onrender.com/service"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
